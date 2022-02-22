b401c0ca16d5d72a1d916a17cb5dca85baed8863
Juli Arianto <97678630+DTFuwyth@users.noreply.github.com>
2022-02-22 13:23:59 +0700

    pertemuan 2

---
+++ Pertemuan 1/JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,13 @@
+package javaapplication4;
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+    }
+    
+}
---
+++ Pertemuan 2/Latihan 1/src/latihan/pkg1/Latihan1.java
@@ -0,0 +1,17 @@
+package latihan.pkg1;
+
+public class Latihan1 {
+    
+    public static void main(String[] args) {
+        String name = "Juli Arianto";
+        int    custID = 1234;
+
+        System.out.println("======================");
+        System.out.println("Selamat Datang");
+        System.out.println("======================");
+        System.out.println("Customer: "+name);
+        System.out.println("Csutomer ID: "+custID);
+
+    }
+    
+}
