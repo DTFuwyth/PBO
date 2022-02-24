a1ccf2784a6e0ac74b14c6360238d8d64b1289b0
Juli Arianto <97678630+DTFuwyth@users.noreply.github.com>
2022-02-24 13:45:39 +0700

    Section 3

---
+++ Section 3/Casting - Parsing/src/Parshing.java
@@ -0,0 +1,14 @@
+package promosi;
+
+public class Parshing {
+    
+    public static void main(String[] args) {
+    String a = "100";
+    String b = "100";
+        System.out.println(a + b);
+
+    int hasil = Integer.parseInt(a) + Integer.parseInt(b);
+        System.out.println(hasil);
+}
+
+}
---
+++ Section 3/Casting - Parsing/src/Promosi.java
@@ -0,0 +1,15 @@
+package promosi;
+
+public class Promosi {
+
+    public static void main(String[] args) {
+        short x = (short)3.0;
+double y = 10.2;
+int z = (int)y;
+
+
+        System.out.println(x);
+        System.out.println(z);
+    }
+    
+}
